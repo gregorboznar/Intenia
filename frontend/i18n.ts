7df7e4a6fb9e71ec18acc9a513a6229a939d27da
Gregor <gregor.boznar@gmail.com>
2025-11-30 22:57:37 +0100

    added locale to i18n

--- frontend/i18n.ts
+++ frontend/i18n.ts
@@ -1,12 +1,13 @@
-import { getRequestConfig } from 'next-intl/server'
+import { getRequestConfig, type RequestConfig } from 'next-intl/server'
 import { notFound } from 'next/navigation'
 
 const locales = ['sl', 'en', 'fr']
 
-export default getRequestConfig(async ({ locale }) => {
+export default getRequestConfig(async ({ locale }): Promise<RequestConfig> => {
   if (!locales.includes(locale as any)) notFound()
 
   return {
+    locale,
     messages: (await import(`./messages/${locale}.json`)).default
   }
 })
