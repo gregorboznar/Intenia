30908fbe40731b4dcfc26c7afd54b3b82f6840f0
Gregor <gregor.boznar@gmail.com>
2025-12-16 20:23:44 +0100

    fix: next-intl

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,28 +1,5 @@
 import createMiddleware from 'next-intl/middleware';
-import { defineRouting } from 'next-intl/routing';
-
-const routing = defineRouting({
-  locales: ['sl', 'en', 'fr'],
-  defaultLocale: 'sl',
-  pathnames: {
-    '/': '/',
-    '/products': {
-      sl: '/izdelki',
-      en: '/products',
-      fr: '/produits'
-    },
-    '/gallery': {
-      sl: '/galerija',
-      en: '/gallery',
-      fr: '/galerie'
-    },
-    '/cookies': {
-      sl: '/piskotki',
-      en: '/cookies',
-      fr: '/cookies'
-    }
-  }
-});
+import { routing } from './routing';
 
 export default createMiddleware(routing);
 
